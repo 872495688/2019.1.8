69d27ae38c84916879835e57a1401afefdae35ab
872495688 <872495688@qq.com>
2019-01-11 10:40:56 +0800

    9

--- ContosoUniversity2/ContosoUniversity2/Controllers/HomeController.cs
+++ ContosoUniversity2/ContosoUniversity2/Controllers/HomeController.cs
@@ -5,27 +5,32 @@ using System.Web;
 using System.Web.Mvc;
 using ContosoUniversity2.DAL;
 using ContosoUniversity2.ViewModels;
+using ContosoUniversity2.comm;
 
 namespace ContosoUniversity2.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
-            return View();
+            var data = WeatherHelper.GetWeatherByCityName("柳州");
+            return View(data);
         }
 
         public ActionResult About()
         {
-            IQueryable<EnrollmentDateGroup> data = from student in db.Students
-                                                   group student by student.EnrollmentDate into dateGroup
-                                                   select new EnrollmentDateGroup()
-                                                   {
-                                                       EnrollmentDate = dateGroup.Key,
-                                                       StudentCount = dateGroup.Count()
-                                                   };
-            return View(data.ToList());
+            ViewBag.Message = "Your application description page";
+            return View();
+            //IQueryable<EnrollmentDateGroup> data = from student in db.Students
+            //                                       group student by student.EnrollmentDate into dateGroup
+            //                                       select new EnrollmentDateGroup()
+            //                                       {
+            //                                           EnrollmentDate = dateGroup.Key,
+            //                                           StudentCount = dateGroup.Count()
+            //                                       };
+            //return View(data.ToList());
         }
 
         public ActionResult Contact()
